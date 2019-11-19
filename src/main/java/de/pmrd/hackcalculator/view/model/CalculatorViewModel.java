6e053e4fa2993604b9c1be659c5e2da40fb9cfa2
Michael Kern <michael.kern@mach.de>
2019-11-19 09:19:07 +0100

    Revert "Add extra ingredients"
    This reverts commit e389a490

--- src/main/java/de/pmrd/hackcalculator/view/model/CalculatorViewModel.java
+++ src/main/java/de/pmrd/hackcalculator/view/model/CalculatorViewModel.java
@@ -1,7 +1,5 @@
 package de.pmrd.hackcalculator.view.model;
 
-import com.vaadin.flow.component.checkbox.Checkbox;
-
 public class CalculatorViewModel {
 
   private double numberOfPersons;
@@ -9,14 +7,6 @@ public class CalculatorViewModel {
   private double hackPerBun;
 
   private double bunsPerPerson;
-
-  private boolean onions;
-
-  private boolean mustard;
-
-  private boolean pickles;
-
-  private boolean toothpick;
 
   public CalculatorViewModel() {
   }
@@ -43,37 +33,5 @@ public class CalculatorViewModel {
 
   public void setBunsPerPerson(double bunsPerPerson) {
     this.bunsPerPerson = bunsPerPerson;
-  }
-
-  public boolean isOnions() {
-    return onions;
-  }
-
-  public void setOnions(boolean onions) {
-    this.onions = onions;
-  }
-
-  public boolean isMustard() {
-    return mustard;
-  }
-
-  public void setMustard(boolean mustard) {
-    this.mustard = mustard;
-  }
-
-  public boolean isPickles() {
-    return pickles;
-  }
-
-  public void setPickles(boolean pickles) {
-    this.pickles = pickles;
-  }
-
-  public boolean isToothpick() {
-    return toothpick;
-  }
-
-  public void setToothpick(boolean toothpick) {
-    this.toothpick = toothpick;
   }
 }
