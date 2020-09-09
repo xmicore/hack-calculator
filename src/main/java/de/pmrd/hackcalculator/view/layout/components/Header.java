c1849aae533a4518092bb07a904e3a7f39053b4f
Michael Kern <michael.kern@mach.de>
2020-09-09 09:27:04 +0200

    Exchange the icon and logo in the branding.

--- src/main/java/de/pmrd/hackcalculator/view/layout/components/Header.java
+++ src/main/java/de/pmrd/hackcalculator/view/layout/components/Header.java
@@ -1,9 +1,9 @@
 package de.pmrd.hackcalculator.view.layout.components;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 
@@ -14,10 +14,21 @@ public class Header extends Composite<FlexLayout> {
   protected FlexLayout initContent() {
     FlexLayout content = new FlexLayout();
     content.setClassName("branding");
-    Icon piggy = VaadinIcon.PIGGY_BANK.create();
-    piggy.setClassName("piggy");
-    content.add(piggy, new Label("Hack-Calculator"));
+    content.add(getLogo(), getLabel());
     content.setAlignSelf(Alignment.CENTER);
     return content;
+  }
+
+  private Span getLogo() {
+    Image logo = new Image("icons/icon.png", "logo");
+    Span span = new Span(logo);
+    span.setClassName("branding__logo");
+    return span;
+  }
+
+  private Label getLabel() {
+    Label label = new Label("Hack-Calculator");
+    label.addClassName("branding_label");
+    return label;
   }
 }
